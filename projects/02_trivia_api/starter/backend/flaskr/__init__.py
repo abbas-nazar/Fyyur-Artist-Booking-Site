e2df6fbb91c27d6704a5a8bb8cfdb3a2699215f3
abbas-nazar <abbasnazar.970@gmail.com>
2021-07-27 00:24:26 +0500

    api created

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,9 +8,21 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
-  app = Flask(__name__)
+  app = Flask(__name__, instance_relative_config=True)
+  CORS(app)
+  CORS(app, resources={r"/api/*": {"origins": "*"}})
   setup_db(app)
   
   '''
@@ -20,12 +32,25 @@ def create_app(test_config=None):
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTION')
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+
+  @app.route('/categories')
+  def retrieve_categories():
+    categories = Category.query.order_by(Category.id).all()
+    categories = {category.id: category.type for category in categories}
+    return jsonify({
+      'categories': categories
+    })
 
 
   '''
@@ -41,6 +66,19 @@ def create_app(test_config=None):
   Clicking on the page numbers should update the questions. 
   '''
 
+  @app.route('/questions')
+  def retrieve_questions():
+    questions = Question.query.order_by(Question.id).all()
+    current_questions = paginate_questions(request, questions)
+    categories = Category.query.order_by(Category.id).all()
+    categories = {category.id: category.type for category in categories}
+
+    return jsonify({
+      'questions': current_questions,
+      'totalQuestions': len(questions),
+      'categories': categories
+    })
+
   '''
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
@@ -48,6 +86,21 @@ def create_app(test_config=None):
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
   '''
+
+  @app.route('/questions/<int:question_id>', methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+      question = Question.query.filter(Question.id == question_id).one_or_none()
+
+      if question is None:
+        abort(404)
+
+      question.delete()
+
+      return jsonify({}), 200
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -59,6 +112,23 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+  @app.route('/questions', methods=['POST'])
+  def create_question():
+    body = request.get_json()
+
+    question = body.get('question', None)
+    answer = body.get('answer', None)
+    difficulty = body.get('difficulty', None)
+    category = body.get('category', None)
+
+    try:
+      question = Question(question=question, answer=answer, difficulty=difficulty, category=category)
+      question.insert()
+
+      return jsonify({}), 200
+
+    except:
+      abort(422)
 
   '''
   @TODO: 
@@ -71,6 +141,19 @@ def create_app(test_config=None):
   Try using the word "title" to start. 
   '''
 
+  @app.route('/questions/search', methods=['POST'])
+  def search_question():
+    body = request.get_json()
+    search_term = body.get('searchTerm', '')
+    search_term = "%{}%".format(search_term)
+    questions = Question.query.filter(Question.question.like(search_term)).all()
+    questions = [question.format() for question in questions]
+
+    return jsonify({
+      'questions': questions,
+      'totalQuestions': len(questions)
+    }), 200
+
   '''
   @TODO: 
   Create a GET endpoint to get questions based on category. 
@@ -79,6 +162,17 @@ def create_app(test_config=None):
   categories in the left column will cause only questions of that 
   category to be shown. 
   '''
+
+  @app.route('/categories/<int:category_id>/questions')
+  def retrieve_category_questions(category_id):
+    questions = Question.query.filter(Question.category == str(category_id)).order_by(Question.id).all()
+    questions = [question.format() for question in questions]
+
+    return jsonify({
+      'questions': questions,
+      'totalQuestions': len(questions),
+      'currentCategory': Category.query.get(category_id).type
+    })
 
 
   '''
@@ -92,13 +186,46 @@ def create_app(test_config=None):
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
+  @app.route('/quizzes', methods=['POST'])
+  def quizzes():
+    body = request.get_json()
+    previous_questions = body.get('previous_questions', [])
+    quiz_category = body.get('quiz_category', '')
+    category_id = quiz_category.get('id')
+    questions = Question.query.filter(~Question.id.in_(previous_questions))
+    if not category_id == 0:
+        questions = questions.filter(Question.category == str(category_id))
+    if not questions:
+      abort(404)
+    questions = questions.all()
+    question = questions[random.randrange(0, len(questions))]
+
+    return jsonify({
+      'question': question.format(),
+    }), 200
 
   '''
   @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
-  
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False,
+      "error": 404,
+      "message": "Not found"
+    }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False,
+      "error": 422,
+      "message": "unprocessable"
+    }), 422
+
   return app
 
     
